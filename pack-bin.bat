d0262800ecc2b2f0ebf434345262c6d589474b9e
wangwenx190 <2546789017@qq.com>
2018-03-07 17:19:17 +0800

    Update.

--- pack-bin.bat
+++ pack-bin.bat
@@ -6,6 +6,7 @@ IF NOT EXIST bin64 (
 )
 SET "PATH=%~dp03rdparty;%PATH%"
 CD /D "%~dp0bin64\release"
+IF EXIST "libGLESV2.dll" upx --best --force "libGLESV2.dll"
 IF EXIST "libmpv.dll" upx --best --force "libmpv.dll"
 IF EXIST "opengl32sw.dll" upx --best --force "opengl32sw.dll"
 IF EXIST "Sugoi.dll" upx --best --force "Sugoi.dll"
@@ -14,4 +15,7 @@ IF EXIST "SugoiGuard.exe" upx --best --force "SugoiGuard.exe"
 IF EXIST "SugoiGuard64.exe" upx --best --force "SugoiGuard64.exe"
 IF EXIST "Sugoi.exe" upx --best --force "Sugoi.exe"
 IF EXIST "Sugoi64.exe" upx --best --force "Sugoi64.exe"
+IF EXIST "WinSparkle.dll" upx --best --force "WinSparkle.dll"
+IF EXIST "youtube-dl.exe" upx --best --force "youtube-dl.exe"
+IF EXIST "Qt*.dll" upx --best --force "Qt*.dll"
 PAUSE
--- src/sugoi/player.pro
+++ src/sugoi/player.pro
@@ -18,3 +18,7 @@ include(src.pri)
 QMAKE_POST_LINK += $$quote(windeployqt \"$${DESTDIR}\\$${TARGET}.exe\"$$escape_expand(\\n\\t))
 
 include(deploy.pri)
+
+QMAKE_POST_LINK += $$quote(copy /y \"$$[QT_INSTALL_PLUGINS]\\platforms\\qdirect2d.dll\" \"$${DESTDIR}\\platforms\\qdirect2d.dll\"$$escape_expand(\\n\\t))
+QMAKE_POST_LINK += $$quote(copy /y \"$$[QT_INSTALL_PLUGINS]\\platforms\\qminimal.dll\" \"$${DESTDIR}\\platforms\\qminimal.dll\"$$escape_expand(\\n\\t))
+QMAKE_POST_LINK += $$quote(copy /y \"$$[QT_INSTALL_PLUGINS]\\platforms\\qoffscreen.dll\" \"$${DESTDIR}\\platforms\\qoffscreen.dll\"$$escape_expand(\\n\\t))
